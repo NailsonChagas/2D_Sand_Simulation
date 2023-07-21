fc7737fb2b0f04d154208e93f9c7ca9f8f20b59a
Nailson Chagas <nailsonchagas@pop-os.localdomain>
2023-07-21 12:37:59 -0300

    feat: adicionado a lógica dos botões/home/nailsonchagas/Nailson/pixels/venv/bin/python

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from Game import Window
 import os
 
 if __name__ == "__main__":
-    simulation = Window("Simulation", os.path.join(".", "grid.json"), True)
+    simulation = Window("Simulation", os.path.join(".", "grid.json"), False)
     simulation.run()
