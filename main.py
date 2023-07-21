06462c1b307cfd0c6bac4cbe6e38da408e02df59
Nailson Chagas <nailsonchagas@pop-os.localdomain>
2023-07-21 00:14:21 -0300

    fix: deixando compativel com o windows

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Game import Window
+import os
 
 if __name__ == "__main__":
-    simulation = Window("Simulation", "./grid.json", False)
+    simulation = Window("Simulation", os.path.join(".", "grid.json"), False)
     simulation.run()
